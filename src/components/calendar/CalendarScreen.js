084760dd06f6eda43fc36e7450da4310b78d3ac2
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-30 00:22:41 -0300

    chore(calendar): added spanish language and styles

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Navbar } from '../ui/Navbar';
 import moment from 'moment';
+import 'moment/locale/es';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { messages } from '../../helpers/calendar-messages-es';
+moment.locale('es');
 
 const localizer = momentLocalizer(moment);
 const events = [
@@ -14,10 +17,27 @@ const events = [
   },
 ];
 export const CalendarScreen = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: '#367CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      display: 'block',
+      color: 'white',
+    };
+    return { style };
+  };
   return (
     <div className="calendar-screen">
       <Navbar />
-      <Calendar localizer={localizer} events={events} startAccessor="start" endAccessor="end" />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
+      />
     </div>
   );
 };
