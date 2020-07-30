7e896610f41564124c22ae84eb9eceb1bb53a6e9
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-30 18:53:18 -0300

    feat(calendarReducer): add new event

--- src/reducers/calendarReducer.js
+++ src/reducers/calendarReducer.js
@@ -21,7 +21,8 @@ export const calendarReducer = (state = initialState, action) => {
   switch (action.type) {
     case types.eventSetActive:
       return { ...state, activeEvent: action.payload };
-
+    case types.eventAddNew:
+      return { ...state, events: [...state.events, action.payload] };
     default:
       return state;
   }
