4f7a00fc39dc61f47021df64d9e01880d85fdb79
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-30 00:03:56 -0300

    feat: implement calendar component with styles

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Navbar } from '../ui/Navbar';
+import moment from 'moment';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
+const localizer = momentLocalizer(moment);
+const events = [
+  {
+    title: 'Cumpleaños del jefe',
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgcolor: '#fafafa',
+  },
+];
 export const CalendarScreen = () => {
   return (
-    <div>
+    <div className="calendar-screen">
       <Navbar />
-      <h1>CalendarScreen</h1>
+      <Calendar localizer={localizer} events={events} startAccessor="start" endAccessor="end" />
     </div>
   );
 };
