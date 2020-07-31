6e6eff2908c1a5823454d3ed9370b9a3e9cc4a53
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-31 12:39:16 -0300

    chore(loginscreen): handle Login Form with useForm

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import './login.css';
+import { useForm } from '../../hooks/useForm';
 
 export const LoginScreen = () => {
+  const [formLoginValues, handleLoginInputChange] = useForm({
+    lEmail: 'luis@luis.com',
+    lPassword: 123456,
+  });
+  const { lEmail, lPassword } = formLoginValues;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(formLoginValues);
+  };
+
   return (
     <div className="container login-container">
       <div className="row">
         <div className="col-md-6 login-form-1">
           <h3>Ingreso</h3>
-          <form>
+          <form onSubmit={handleLogin}>
             <div className="form-group">
-              <input type="text" className="form-control" placeholder="Correo" />
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Correo"
+                name="lEmail"
+                value={lEmail}
+                onChange={handleLoginInputChange}
+              />
             </div>
             <div className="form-group">
-              <input type="password" className="form-control" placeholder="Contraseña" />
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Contraseña"
+                name="lPassword"
+                value={lPassword}
+                onChange={handleLoginInputChange}
+              />
             </div>
             <div className="form-group">
               <input type="submit" className="btnSubmit" value="Login" />
