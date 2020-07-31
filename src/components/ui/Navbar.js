f35dc7876953b7e0d59bbf932a7c204eac6e5632
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-31 18:00:48 -0300

    feat: logout users

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,12 +1,18 @@
 import React from 'react';
-
+import { useSelector, useDispatch } from 'react-redux';
+import { startLogout } from '../../actions/auth';
 export const Navbar = () => {
+  const { name } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(startLogout());
+  };
   return (
     <div className="navbar navbar-dark bg-dark mb-4">
-      <span className="navbar-brand">Luis</span>
+      <span className="navbar-brand">{name}</span>
       <button className="btn btn-outline-danger">
         <i className="fas fa-sign-out-alt"></i>
-        <span> Salir</span>
+        <span onClick={handleLogout}> Salir</span>
       </button>
     </div>
   );
