c39d3d3ee4c7e1de142f372cd8aa0dca500f45de
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-08-01 09:11:40 -0300

    test: should fetchWithToken fail when trying to delete an event

--- src/helpers/fetch.test.js
+++ src/helpers/fetch.test.js
@@ -1,6 +1,8 @@
-const { fetchSinToken } = require('./fetch');
+const { fetchSinToken, fetchConToken } = require('./fetch');
 
 describe('Pruebas en el helper fetch', () => {
+  let token = '';
+
   test('fetchSinToken debe de funcionar', async () => {
     const resp = await fetchSinToken(
       'auth',
@@ -10,5 +12,12 @@ describe('Pruebas en el helper fetch', () => {
     expect(resp instanceof Response).toBe(true);
     const body = await resp.json();
     expect(body.ok).toBe(true);
+    token = body.token;
+  });
+  test('fetchConToken debe de funcionar', async () => {
+    localStorage.setItem('token', token);
+    const resp = await fetchConToken('events/5f23c45b1363046c94b3f9e6', {}, 'DELETE');
+    const body = await resp.json();
+    expect(body.msg).toBe('Evento no existe con ese id');
   });
 });
