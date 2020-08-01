5392b8631e1d887e7cc365eedac5b53a83ff065d
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-08-01 10:33:43 -0300

    test: should authenticate the user

--- src/tests/reducers/authReducer.test.js
+++ src/tests/reducers/authReducer.test.js
@@ -1,10 +1,21 @@
-const initialState = {
+const { types } = require('../../types/types');
+const { authReducer } = require('../../reducers/authReducer');
+
+const initState = {
   checking: true,
 };
 describe('Pruebas en el authReducer', () => {
   test('debe de retornar el estado por defecto', () => {
     const action = {};
-    const state = authReducer(initialState, action);
-    expect(state).toEqual(initialState);
+    const state = authReducer(initState, action);
+    expect(state).toEqual(initState);
+  });
+  test('deberia autenticar el usuario', () => {
+    const action = {
+      type: types.authLogin,
+      payload: { uid: '123', name: 'Luis' },
+    };
+    const state = authReducer(initState, action);
+    expect(state).toEqual({ checking: false, uid: '123', name: 'Luis' });
   });
 });
