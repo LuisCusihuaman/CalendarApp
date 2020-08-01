7e2b542d7c524ba089c3cb28995ce04c4b8ac7c5
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-08-01 11:33:08 -0300

    test: should loginscreen call the login dispatch

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -8,8 +8,8 @@ export const LoginScreen = () => {
   const dispatch = useDispatch();
 
   const [formLoginValues, handleLoginInputChange] = useForm({
-    lEmail: 'luis@luis.com',
-    lPassword: '123456',
+    lEmail: '',
+    lPassword: '',
   });
   const { lEmail, lPassword } = formLoginValues;
 
--- src/tests/components/auth/LoginScreen.test.js
+++ src/tests/components/auth/LoginScreen.test.js
@@ -5,6 +5,9 @@ import configureStore from 'redux-mock-store'; //ES6 modules
 import thunk from 'redux-thunk';
 import '@testing-library/jest-dom';
 import { LoginScreen } from '../../../components/auth/LoginScreen';
+import { startLogin } from '../../../actions/auth';
+
+jest.mock('../../../actions/auth', () => ({ startLogin: jest.fn() }));
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -20,5 +23,23 @@ const wrapper = mount(
 describe('Pruebas en <LoginScreen/>', () => {
   test('debe de mostrarse correctamente', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+  test('debe de llamar el dispatch del login', () => {
+    wrapper.find("input[name='lEmail']").simulate('change', {
+      target: {
+        name: 'lEmail',
+        value: 'test3@test3.com',
+      },
+    });
+    wrapper.find("input[name='lEmail']").simulate('change', {
+      target: {
+        name: 'lPassword',
+        value: '123456',
+      },
+    });
+    wrapper.find('form').at(0).prop('onSubmit')({
+      preventDefault() {},
+    });
+    expect(startLogin).toHaveBeenCalledWith('test3@test3.com', '123456');
   });
 });
