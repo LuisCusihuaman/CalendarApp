0e0b002946012a6b165906830efd0ea9040efa38
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-08-01 11:07:02 -0300

    test: should AppRouter show the 'espere'

---
+++ src/tests/router/AppRouter.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store'; //ES6 modules
+import thunk from 'redux-thunk';
+import '@testing-library/jest-dom';
+import { AppRouter } from '../../router/AppRouter';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+describe('Pruebas en <AppRouter/>', () => {
+  test('debe de mostrar el espere', () => {
+    const initialState = { auth: { checking: true } };
+    const store = mockStore(initialState);
+    const wrapper = mount(
+      <Provider store={store}>
+        <AppRouter />
+      </Provider>,
+    );
+    expect(wrapper.find('h5').exists()).toBe(true);
+  });
+});
